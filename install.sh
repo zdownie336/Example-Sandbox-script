7d9a1b0df5b7c8d7ccb41279041a31e00343b851
Sebastion Price <sebastionprice@gmail.com>
2026-07-02 13:57:18 +1000

    Fix path

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ SCRIPTREPO="Example-Sandbox-script"
 
 
 echo "hello, getting bashrc and saving your old one";
-wget -qO ~/.bashrc https://raw.githubusercontent.com/{$USERNAME}/{$SCRIPTREPO}/refs/heads/main/bashrc;
+https://raw.githubusercontent.com/{$USERNAME}/{$SCRIPTREPO}/refs/heads/main/bashrc
